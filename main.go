0927d1c2712f8a5f07d0b0f1d262c380a29058b9
Kevin Schweikert <kevin@kevinschweikert.de>
2020-05-01 17:05:58 +0200

    Restructuring and Feature completion

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -16,69 +17,144 @@ import (
 	"github.com/faiface/beep/effects"
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep/wav"
 )
 
-var soundBtn []fyne.CanvasObject
-var volumeLabel *widget.Label
-var files []soundfile
-var mainVol float64
-
-type soundfile struct {
-	Path string
-	Name string
+type soundFile struct {
+	path      string
+	name      string
+	extension string
 }
 
-func main() {
-	mainVol = 0
+type soundDirectory struct {
+	soundFiles []soundFile
+	path       string
+}
 
-	folderPath := flag.String("path", "./sounds", "Path to sound files")
-	sampleRate := flag.Int("samplerate", 48000, "Output Samplerate in Hz")
-	buffSize := flag.Int("buff", 256, "Output buffer size in bytes")
-	flag.Parse()
+type audioPanel struct {
+	speakerSampleRate beep.SampleRate
+	mixer             *beep.Mixer
+	ctrl              *beep.Ctrl
+	volume            *effects.Volume
+	err               error
+}
 
-	speaker.Init(beep.SampleRate(*sampleRate), *buffSize)
+func newAudioPanel(speakerSampleRate beep.SampleRate) *audioPanel {
+	err := errors.New("")
+	mixer := &beep.Mixer{}
+	ctrl := &beep.Ctrl{Streamer: mixer}
+	volume := &effects.Volume{Streamer: mixer, Base: 2}
+	return &audioPanel{speakerSampleRate, mixer, ctrl, volume, err}
+}
 
-	err := filepath.Walk(*folderPath, onFileFound(&files))
+func (ap *audioPanel) play(streamer beep.StreamSeekCloser, audioSampleRate beep.SampleRate) {
+	ap.stop()
+	resampler := beep.Resample(4, audioSampleRate, ap.speakerSampleRate, streamer)
+	ap.mixer.Add(resampler)
+
+}
+
+func (ap *audioPanel) stop() {
+	ap.mixer.Clear()
+}
+
+func (ap *audioPanel) changeVolume(newVolume float64) {
+	speaker.Lock()
+	ap.volume.Volume = newVolume
+	speaker.Unlock()
+}
+
+func (ap *audioPanel) init(buffSize int) {
+	err := speaker.Init(ap.speakerSampleRate, buffSize)
+	if err != nil {
+		log.Fatal("Could not initialize speaker")
+	}
+	speaker.Play(ap.volume)
+}
+
+func getFilesInFolder(folderPath string) soundDirectory {
+	var files []soundFile
+	err := filepath.Walk(folderPath, onFileFound(&files))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, file := range files {
-		name := file.Name
-		path := file.Path
-		newSoundBtn := widget.NewButton(name, func() { buttonCallback(path) })
-		soundBtn = append(soundBtn, newSoundBtn)
+	return soundDirectory{files, folderPath}
+}
+
+func onFileFound(files *[]soundFile) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			filenameParts := strings.Split(info.Name(), ".")
+			filename := filenameParts[0]
+			extension := strings.ToLower(filenameParts[len(filenameParts)-1])
+			*files = append(*files, soundFile{path, filename, extension})
+		}
+		return nil
 	}
+}
+
+func (s *soundDirectory) getSoundButtons(ap *audioPanel) []fyne.CanvasObject {
+	var buttons []fyne.CanvasObject
+	for _, file := range s.soundFiles {
+		name := file.name
+		soundfile := file
+		button := widget.NewButton(name, func() { playSound(ap, soundfile) })
+		buttons = append(buttons, button)
+	}
+
+	return buttons
+}
+
+func main() {
+
+	folderPath := flag.String("path", "./sounds", "path to sound files")
+	speakerSampleRate := flag.Int("samplerate", 48000, "Output Samplerate in Hz")
+	buffSize := flag.Int("buff", 256, "Output buffer size in bytes")
+	flag.Parse()
+
+	ap := newAudioPanel(beep.SampleRate(*speakerSampleRate))
+	ap.init(*buffSize)
+
+	dir := getFilesInFolder(*folderPath)
+	buttons := dir.getSoundButtons(ap)
 
 	a := app.New()
 	w := a.NewWindow("Soundboard")
 
+	volumeLabel := widget.NewLabel(fmt.Sprintf("%.1f", ap.volume.Volume))
+	volumeLabel.TextStyle.Bold = true
+
 	volumeSlider := widget.NewSlider(-5, 0)
 	volumeSlider.Value = 0
-	volumeSlider.Step = 0.1
+	volumeSlider.Step = 0.05
 	//volumeSlider.Value = 0
-	volumeSlider.OnChanged = volumeChanged
+	volumeSlider.OnChanged = onVolumeChanged(ap, volumeLabel)
 
-	volumeLabel = widget.NewLabel(fmt.Sprintf("%.1f", mainVol))
+	errorLabel := widget.NewLabel(ap.err.Error())
 
-	closeBtn := widget.NewButton("Close", func() { w.Close() })
+	stopButton := widget.NewButton("Stop All", func() { ap.stop() })
+	stopButton.Style = widget.PrimaryButton
 
 	buttonContainer := fyne.NewContainerWithLayout(
 		layout.NewGridLayoutWithColumns(5),
-		soundBtn...,
+		buttons...,
 	)
 
-	closeContainer := fyne.NewContainerWithLayout(
-		layout.NewCenterLayout(),
-		closeBtn,
+	volumeContainer := widget.NewVBox(
+		volumeLabel,
+		volumeSlider,
+		stopButton,
+		errorLabel,
 	)
 
 	appContainer := fyne.NewContainerWithLayout(
-		layout.NewGridLayout(1),
+		layout.NewGridLayoutWithColumns(1),
 		buttonContainer,
-		closeContainer,
-		volumeLabel,
-		volumeSlider,
+		volumeContainer,
 	)
 
 	w.SetContent(appContainer)
@@ -87,42 +163,45 @@ func main() {
 	w.ShowAndRun()
 }
 
-func onFileFound(files *[]soundfile) filepath.WalkFunc {
-	return func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			filename := strings.Split(info.Name(), ".")[0]
-			*files = append(*files, soundfile{path, filename})
-		}
-		return nil
-	}
-}
+func playSound(ap *audioPanel, file soundFile) {
+	var streamer beep.StreamSeekCloser
+	var format beep.Format
+	var err error
 
-func buttonCallback(path string) {
-	f, err := os.Open(path)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	streamer, format, err := mp3.Decode(f)
+	f, err := os.Open(file.path)
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
 
-	resampled := beep.Resample(4, format.SampleRate, 48000, streamer)
-	volume := &effects.Volume{
-		Streamer: resampled,
-		Base:     2,
-		Volume:   mainVol,
-		Silent:   false,
+	switch file.extension {
+	case "mp3":
+		streamer, format, err = mp3.Decode(f)
+		if err != nil {
+			fmt.Println(err)
+			ap.err = err
+			return
+		}
+	case "wav":
+		streamer, format, err = wav.Decode(f)
+		if err != nil {
+			fmt.Println(err)
+			ap.err = err
+			return
+		}
+	default:
+		fmt.Println("File format not supported")
+		ap.err = errors.New("File format not supported")
+		return
 	}
-	speaker.Play(beep.Seq(volume, beep.Callback(func() { streamer.Close() })))
+
+	ap.play(streamer, format.SampleRate)
 }
 
-func volumeChanged(volume float64) {
-	mainVol = volume
-	volumeLabel.Text = fmt.Sprintf("%.1f", mainVol)
-	volumeLabel.Refresh()
+func onVolumeChanged(ap *audioPanel, volumeLabel *widget.Label) func(float64) {
+	return func(volume float64) {
+		ap.changeVolume(volume)
+		volumeLabel.Text = fmt.Sprintf("%.1f", ap.volume.Volume)
+		volumeLabel.Refresh()
+	}
 }
